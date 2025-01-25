9a59e145c08338575aaeb21f536ae970073712ae
William <williamsteve216@gmail.com>
2025-01-25 08:44:29 +0100

    Utilisation de l'héritage

--- Exercice/src/com/inheritance/Reservation.java
+++ Exercice/src/com/inheritance/Reservation.java
@@ -4,4 +4,10 @@ public class Reservation {
     public String customerName;
     public Vehicle vehicle;
     public int rentalDuration;
+
+    public Reservation(String customerName, Vehicle vehicle, int rentalDuration) {
+        this.customerName = customerName;
+        this.vehicle = vehicle;
+        this.rentalDuration = rentalDuration;
+    }
 }
